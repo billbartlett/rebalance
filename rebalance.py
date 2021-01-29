354c942e897e0ce5970c139e60e8b8dccd67142c
Bill Bartlett <bbartlett@gmail.com>
2021-01-28 22:01:52 -0800

    fixed disk finding..  janky as it is.

--- rebalance.py
+++ rebalance.py
@@ -1,21 +1,30 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import os,re
 
-def find_disks(rootdir,depth):
-    """
-    which disks are available to unraid.  to start, this is going to be janky
-    and simply use /mnt/ with a max depth of 1, and match disk*
-    """
-    depth -= 1
-    print(rootdir + "abcd")
-    regex = re.compile('disk*')
-    with os.scandir(rootdir) as p:
-        for entry in p:
-            yield entry.path
-            if entry.is_dir() and depth > 0:
-                if regex.match(entry):
-                    print(entry)
+"""
+which disks are available to unraid.  to start, this is going to be janky
+and simply use /mnt/ with a max depth of 1, and match disk*
+"""
 
+def find_disks(rootdir, depth):
+    disks = []
+    pattern = "(disk[0-9])"
+    with os.scandir(rootdir) as p:
+        depth -= 1
+        for entry in p:
+            #yield entry.path
+            if entry.is_dir() and depth > 0:
+                diskmatch = re.search(pattern, str(entry))
+                if diskmatch:
+                    # <DirEntry 'disk8'>
+                    # extract disk name from string similar to above utilizing the grouping from pattern string
+                    disk_name = diskmatch.group(1)
+                    disks.append(disk_name)
+    return(disks)
 
 rootdir = "/mnt/"
-find_disks(rootdir, 2)
+disklist = find_disks(rootdir, 2)
+
+print("-----------")
+for x in disklist:
+    print(x)

c94a82d2180ddfef329c50e744cbf91328b06885
billbartlett <bbartlett@gmail.com>
2021-01-28 20:32:33 -0800

    getting this repo started

---
+++ rebalance.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import os,re
+
+"""
+define root directory.
+ - using the unraid-specific share directory of /mnt/user0
+"""
+
+def find_disks(rootdir,depth):
+    """
+    which disks are available to unraid.  to start, this is going to be janky
+    and simply use /mnt/disk*
+    """
+    depth -= 1
+    print(rootdir + "abcd")
+    regex = re.compile('disk*')
+    with os.scandir(rootdir) as p:
+        for entry in p:
+            yield entry.path
+            if entry.is_dir() and depth > 0:
+                print(entry)
+                if regex.match(entry):
+                    print(entry)
+
+
+rootdir = "/mnt/"
+find_disks(rootdir, "2")

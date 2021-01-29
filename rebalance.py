b68f4508aa832650ed844e96fe8e397a85c66234
Bill Bartlett <bbartlett@splunk.com>
2021-01-29 02:02:15 -0800

    added function for directory listing and tree size

--- rebalance.py
+++ rebalance.py
@@ -2,19 +2,13 @@
 import os,re
 
 """
-Hopefully, eventually, this will be a tool I can use to balance unraid shares.
-The idea is that when adding new drives, most new data is going to be exclusively
-written to the new drives unless some sort of balancing is done.  This is my
-attempt to write my own utility to do said balancing.
+Hopefully, eventually, this will be a tool I can use to balance unraid shares. The idea is that when adding new drives, most new data is going to be exclusively written to the new drives unless some sort of balancing is done.  This is my attempt to write my own utility to do said balancing.
 """
 
-disklist = []
-sharelist = []
 
 def find_disks(depth):
     """
-    which disks are available to unraid.  to start, this is going to be janky
-    and simply use /mnt/ with a max depth of 1, and match disk*
+    which disks are available to unraid.  to start, this is going to be janky and simply use /mnt/ with a max depth of 1, and match disk*
     """
     rootdir = "/mnt"
     disks = []
@@ -35,8 +29,7 @@ def find_disks(depth):
 
 def get_shares(depth):
     """
-    this is pretty janky, again, but simply grab the list of directories under /mnt/user0, an
-    an unraid-specific shortcut to access shares
+    this is pretty janky, again, but simply grab the list of directories under /mnt/user0, an an unraid-specific shortcut to access shares
     """
     rootdir = "/mnt/user0/"
     shares = []
@@ -48,27 +41,72 @@ def get_shares(depth):
             if entry.is_dir() and depth > 0:
                 sharematch = re.search(pattern, str(entry))
                 if sharematch:
-                    # extract share name utilizing the grouping from pattern string, and remove single quotes
+                    # extract share name utilizing the grouping regex and remove single quotes
                     share_name = sharematch.group(1)
                     share_name = str(share_name.replace("'",""))
                     shares.append(share_name)
     shares.sort()
     return(shares)
 
+def get_dirs(basedir):
+    """
+    build a list of all subdirectories in all shares, with a depth limit of 1
+
+    depth is a method to limit how far down the directory structure the code will look.  I'm only interested in 1 level after the share name:
+        /mnt/disk1/movies/this_directory_is_calculated
+
+    since dirname will be /mnt/[disk_name]/[share_name] I only want to go one level deeper.  set depth as 2, and the first iteration will lower it to 1, and the code will stop once depth is not > 0.
+    """
+
+    # os.scandir() will encapsulate the directory name in single quotes
+    pattern = "('.*')"
+    depth = 3
+    # does the directory exist?
+    isDirectory = os.path.isdir(dirname)
+    if isDirectory:
+        with os.scandir(dirname) as p:
+            depth -= 1
+            for entry in p:
+                # no symlinks
+                if not os.path.islink(entry):
+                    if entry.is_dir() and depth > 0:
+                        dirmatch = re.search(pattern, str(entry))
+                        if dirmatch:
+                            # extract directory name utilizing the grouping regex, and remove single quotes
+                            matched_dir = dirmatch.group(1)
+                            matched_dir = str(matched_dir.replace("'",""))
+                            full_dir_name = dirname + "/" + matched_dir
+                            # append directory to dirlist
+                            dirlist.append(full_dir_name)
+
+def get_tree_size(path):
+    """total size of files in given path and subdirs."""
+    total = 0
+    for entry in os.scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            total += get_tree_size(entry.path)
+        else:
+            total += entry.stat(follow_symlinks=False).st_size
+
+    return total
 
 
 
+disklist = []
+sharelist = []
+dirlist = []
+dirstats = {}
+
+# find the disks and shares
 disklist = find_disks(2)
 sharelist = get_shares(2)
 
+# find the top level directories for each share, on each disk
+for sharename in sharelist:
+    for diskname in disklist:
+        dirname = "/mnt/" + diskname + "/" + sharename
+        get_dirs(dirname)
 
-
-
-
-#for x in disklist:
+#for x in dirlist:
 #    print(x)
-#
-#for x in sharelist:
-#    print(x)
-
 
